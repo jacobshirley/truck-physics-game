61423a41b3605e4b5c2e9972695a9172d918a6a5
Jacob Shirley <jakeshirley2@gmail.com>
2019-11-10 22:56:21 +0000

    Truck now works with physics, but need to get friction right so the wheels rotate.

--- public/script.js
+++ public/script.js
@@ -11,7 +11,8 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update
     }
 };
 
@@ -30,42 +31,88 @@ function preload ()
     this.load.json('shapes', 'assets/truck/truck.json');
 }
 
+var aKey;
+var dKey;
+
+var chassis;
+var backWheel;
+
 function create ()
 {
     var shapes = this.cache.json.get('shapes');
-
-    this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
-
-    this.matter.add.rectangle(0, game.config.height, game.config.width, 100, { restitution: 0.9, isStatic: true });
-
-    this.objs = [];
-
-    this.objs.push(this.matter.add.image(200, 0, 'trucks', {shape: shapes.truck}));
-    this.objs.push(this.matter.add.image(250, 50, 'wheel', {shape: shapes.wheel}));
-
-    var cat1 = this.matter.world.nextCategory();
-
-    //this.objs[0].body.collisionFilter = { group: cat1 };
-    //this.objs[1].body.collisionFilter = { group: cat1 };
 
     var Body = Phaser.Physics.Matter.Matter.Body;
     var Bodies = Phaser.Physics.Matter.Matter.Bodies;
     var Composite = Phaser.Physics.Matter.Matter.Composite;
 
+    this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+
+
+    this.objs = [];
+
+    console.log(shapes.truck);
+
+    this.objs.push(this.matter.add.image(200, 0, 'trucks', {shape: shapes.truck, density: shapes.truck.density }));
+    this.objs.push(this.matter.add.image(200 + 155, 95, 'wheel', {shape: shapes.wheel, frictionStatic: 0.2}));
+    this.objs.push(this.matter.add.image(200 - 155, 95, 'wheel', {shape: shapes.wheel, frictionStatic: 0.2}));
+
+    chassis = this.objs[0];
+    backWheel = this.objs[2];
+
+    var cat1 = this.matter.world.nextCategory();
+
+    this.objs[0].setCollisionCategory(cat1);
+    this.objs[0].setDensity(shapes.truck.density);
+
+    this.objs[1].setCollisionCategory(cat1);
+    this.objs[1].setDensity(shapes.wheel.density);
+    this.objs[1].setFriction(shapes.wheel.friction);
+    Body.set(this.objs[1].body, "frictionStatic", 0.8);
+
+    this.objs[2].setCollisionCategory(cat1);
+    this.objs[2].setDensity(shapes.wheel.density);
+    this.objs[2].setFriction(shapes.wheel.friction);
+    Body.set(this.objs[2].body, "frictionStatic", 0.8);
+
+    let ground = this.matter.add.rectangle(game.config.width / 2, game.config.height, game.config.width, 100, { restitution: 1, isStatic: true, friction: 0.8, frictionStatic: 0.2 });
+
+    Body.set(ground, "friction", 0.8);
+    Body.set(ground, "frictionStatic", 0.8);
+    console.log(ground.friction);
+
+    var cat2 = this.matter.world.nextCategory();
+    ground.collisionFilter.category = cat2;
+
+    this.objs[0].setCollidesWith([ cat2 ]);
+    this.objs[1].setCollidesWith([ cat2 ]);
+    this.objs[2].setCollidesWith([ cat2 ]);
+
+
+
     var composite = Composite.create();
 
-    Composite.addBody(composite, this.objs[0].body);
-    Composite.addBody(composite, this.objs[1].body);
+    //Composite.addBody(composite, this.objs[0].body);
+    //Composite.addBody(composite, this.objs[1].body);
+    //Composite.addBody(composite, this.objs[2].body);
 
-    this.matter.add.constraint(this.objs[0].body, this.objs[1].body, 10, 0.2, {
-      pointA: { x: 10, y: 0 }
-    });
+    const spring = 0.5;
 
-    this.matter.world.add(composite);
+    this.matter.add.constraint(this.objs[0].body, this.objs[1].body, 5, spring, { pointA: { x: 155, y: 95 } });
+    this.matter.add.constraint(this.objs[0].body, this.objs[2].body, 5, spring, { pointA: { x: -115, y: 95 } });
+
+    //this.matter.world.add(composite);
 
     for (var obj of this.objs) {
-        obj.setScale(0.1, 0.1);
+        obj.setScale(0.3, 0.3);
     }
 
+    aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     //this.add.image(50, 50, 'trucks');
+}
+
+function update() {
+    if (aKey.isDown) {
+        chassis.applyForceFrom(chassis.body.position, new Phaser.Math.Vector2(50, 90));
+    }
 }
