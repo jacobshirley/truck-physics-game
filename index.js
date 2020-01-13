3f8fdb35ae14db301df7f8db5850cb6b13c0214c
Jake Shirley <jake.shirley@simplyvat.com>
2020-01-13 11:22:07 +0000

    Server update.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(80, () => console.log("Server started!"));
+app.listen(80, () => console.log("Server started! Visit http://localhost to play the game!"));
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 
-
+const Phaser = require("phaser");
 
 const TruckLoader = require("./truck.js");
 
